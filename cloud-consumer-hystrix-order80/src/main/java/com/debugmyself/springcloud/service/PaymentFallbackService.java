6b9f480819b3ded1c0bfbf895a7de2481eff5b8c
debugmyself <xinquanw@yeah.net>
2022-06-16 16:05:41 +0800

    GateWay

---
+++ cloud-consumer-hystrix-order80/src/main/java/com/debugmyself/springcloud/service/PaymentFallbackService.java
@@ -0,0 +1,7 @@
+package com.debugmyself.springcloud.service;/** 
+* 功能描述
+* 
+* @author wangxq
+* @date 2022年06月14日 16:03
+*/public class PaymentFallbackService {
+}
---
+++ cloud-gateway-gateway9527/src/main/java/com/debugmyself/springcloud/GateWayMain9527.java
@@ -0,0 +1,13 @@
+package com.debugmyself.springcloud;
+
+/**
+ * 功能描述
+ *
+ * @author wangxq
+ * @date ${YEAR}年${MONTH}月${DAY}日 ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

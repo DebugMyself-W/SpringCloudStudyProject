a7a0c627e0cb2d414c8d69c5015cf035783fcc94
debugmyself <xinquanw@yeah.net>
2022-07-07 21:53:03 +0800

    springboot整合redis

---
+++ redis-service/src/main/java/com/debugmyself/cloud/RedisSpringbootApplication.java
@@ -0,0 +1,17 @@
+package com.debugmyself.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述
+ *
+ * @author wangxq
+ * @date 2022年07月07日 21:36
+ */
+@SpringBootApplication
+public class RedisSpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSpringbootApplication.class, args);
+    }
+}
